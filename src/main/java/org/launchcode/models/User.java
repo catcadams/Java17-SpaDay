04986f6c833fbf1c91ee835514566af291ebdd91
catcadams <catceciliaadams@gmail.com>
2024-12-19 20:06:35 -0600

    spa day validation studio

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,42 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "This cannot be blank.")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "This cannot be blank.")
+    @Size(min=6, message = "Password must be at least 6 characters.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = password;
+    }
+
+    private void checkPassword(){
+        if(this.password != null && this.verifyPassword != null && !this.password.equals(this.verifyPassword)) {
+            this.verifyPassword = null;
+        }
     }
 
     public String getUsername() {
@@ -38,6 +61,16 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
 
